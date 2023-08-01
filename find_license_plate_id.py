be550b5a412429d864256cdb8a098b72e14cd543
L0kizz <belaumlauf@live.de>
2023-08-01 21:09:15 +0200

    added requirements file

--- find_license_plate_id.py
+++ find_license_plate_id.py
@@ -4,36 +4,43 @@ import os
 import cv2
 import pytesseract
 import torch
-def main():
-    path_to_cam_frames = 'cam_frames'
-    all_lp_ids = []
-    img_files = [os.path.join(path_to_cam_frames, f) for f in os.listdir(path_to_cam_frames)
-                 if f.endswith('.jpg') or f.endswith('.png')]
-    all_lp_ids = map(find_license_plate_id, img_files)
-    all_lp_ids = [lp_id for lp_id in all_lp_ids if lp_id is not None]
-    predicted_license_plate_id = max(set(all_lp_ids), key=all_lp_ids.count)
-    print(predicted_license_plate_id,"test")
-
+import time
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 #trained yolo model
 lp_detect_model = YOLO('yolo_model/best.pt')
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 lp_detect_model.to(device)
+
+def main():
+    start = time.time()
+    path_to_cam_frames = 'cam_frames'
+    all_lp_ids = []
+    img_files = [os.path.join(path_to_cam_frames, f) for f in os.listdir(path_to_cam_frames)
+                if f.endswith('.jpg') or f.endswith('.png')]
+    all_lp_ids = map(find_license_plate_id, img_files)
+    all_lp_ids = [lp_id for lp_id in all_lp_ids if lp_id is not None]
+    predicted_license_plate_id = max(set(all_lp_ids), key=all_lp_ids.count)
+    print(predicted_license_plate_id)
+    end = time.time()
+    print(f"Time: {end - start}")
+
+
 
 #crop the license plate and save as image
 def crop_license_plates(img, yolo_model, save_img=False):
     img_name = img.split("\\")[-1]
     
-    print(f"Processing {img_name}")
-    detected_lps = yolo_model.predict(img)[0]
+    detected_lps = yolo_model.predict(img, verbose=False)[0]
     license_plates = []
     
     for lp in detected_lps.boxes.data.tolist():
         
         x1, y1, x2, y2, conf, _ = lp
-       
+        
         img_array = cv2.imread(img)
         cropped_lp =img_array[int(y1):int(y2), int(x1):int(x2), :]
         license_plates.append(cropped_lp)
@@ -87,7 +94,7 @@ def preprocess_license_plate(license_plate, name, save_img=False):
     return license_plate_cropped
 
 def find_license_plate_id(img_file):
-   
+
     detected_licenseplates = crop_license_plates(img_file, lp_detect_model)
     assert len(detected_licenseplates) == 1, "WARNING! More than one License plate detected. Access denied!"
     re_cropped = preprocess_license_plate(detected_licenseplates[0], img_file, save_img=False)
