8d9860918375a62ae265d17e45a391e026609220
ElectricUnit <apatonole@gmail.com>
2023-08-03 00:21:37 +0200

    added testing file above scripts and ui

--- mainTestingFile.py
+++ mainTestingFile.py
@@ -1,6 +1,13 @@
-import matching
-import pickle
+from PythonScripts import matching
+from UI_Folder import UI
 import customtkinter
+import pickle
+
+app_instance = UI.App()
+
+app_instance.mainloop()
+
+app_instance.upload_image_to_database("TestImagesSet1\image_1.jpg", True, "SHG-FF-206")
 
 matching.save_contour("PythonScripts\cam_frames\image_11.JPG")
 
@@ -8,6 +15,3 @@ with open('reference_contour.pkl', 'rb') as f:
     contour = pickle.load(f)
 
 matching.compare_ContourImage(contour,"PythonScripts\cam_frames\image_9.JPG")
-
-
-
